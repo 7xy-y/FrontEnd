56fbcf7a331679eac50396ddf75089db5217b6b6
Dummy Head <72564247+DummyNodeHead@users.noreply.github.com>
2021-06-28 15:25:08 +0800

    Delete src directory

--- src/components/cookies.js
+++
@@ -1,9 +0,0 @@
-import cookies from 'react-cookies'
-
-export function setToken(value){
-    cookies.save("adminToken",value);
-}
-
-export function setUsername(value){
-    cookies.save("username",value);
-}
